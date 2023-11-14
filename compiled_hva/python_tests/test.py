406b71fc94f2e0528c2014ded06e0a2aee917755
Chae-Yeun Park <chae-yeun@Xanadu.ai>
2023-11-13 20:46:52 -0500

    Add cpp code

---
+++ compiled_hva/python_tests/test.py
@@ -0,0 +1,44 @@
+import pennylane as qml
+import numpy as np
+import pennylane.numpy as pnp
+
+dev = qml.device('lightning.qubit', wires=16)
+
+
+def create_circuit(num_blocks, edges):
+
+    ops = []
+    coeffs = []
+
+    for edge in edges:
+        ops.append(qml.PauliX(edge[0]) @ qml.PauliX(edge[1]))
+        ops.append(qml.PauliY(edge[0]) @ qml.PauliY(edge[1]))
+        ops.append(qml.PauliZ(edge[0]) @ qml.PauliZ(edge[1]))
+        coeffs.append(1.0)
+        coeffs.append(1.0)
+        coeffs.append(1.0)
+
+    ham = qml.Hamiltonian(coeffs, ops)
+        
+    @qml.qnode(dev)
+    def circuit(params):
+        for k in range(num_blocks):
+            for edge in edges:
+                qml.IsingXX(params[3*k+0], wires=edge)
+            for edge in edges:
+                qml.IsingYY(params[3*k+1], wires=edge)
+            for edge in edges:
+                qml.IsingZZ(params[3*k+2], wires=edge)
+
+        return qml.expval(ham)
+    return circuit
+
+edges = []
+for i in range(16):
+    edges.append([i, (i+1)%16])
+
+my_circuit = create_circuit(3, edges)
+params = pnp.array(0.2*np.ones(9), requires_grad=True)
+
+print(my_circuit(params))
+print(qml.grad(my_circuit)(params))
---
+++ compiled_hva/test/test.cpp
@@ -0,0 +1,27 @@
+#include "hva_xyz.hpp"
+#include <iostream>
+#include <numbers>
+
+int main() {
+	std::vector<std::pair<size_t, size_t>> edges;
+	for(size_t i = 0; i < 16; i++) {
+		edges.emplace_back(i, (i+1)%16);
+	}
+
+	HVA hva(16, 3, std::span{edges});
+
+	std::vector<double> params(9, 0.2);
+
+	std::vector<std::complex<double>> ini_st(1u << 16, 0.0);
+	ini_st[0] = 1.0;
+
+	std::cout << hva.expval_shots(std::span{ini_st}, std::span{params}, 1'000'000) << std::endl;
+
+	auto grads = hva.grad_shots(std::span{ini_st}, std::span{params}, 1'000'000);
+	for(const auto g: grads) {
+		std::cout << g << ", ";
+	}
+	std::cout << std::endl;
+
+	return 0;
+}
